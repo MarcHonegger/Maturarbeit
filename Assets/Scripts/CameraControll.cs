9c5646ab8aa1b9b43979e1daef686af86e33d2f8
MarcHonegger <marchonegger@icloud.com>
2022-03-23 14:04:39 +0100

    Camera Minor Changes
    Size
    Speed
    Code small changes

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class CameraControll : MonoBehaviour
 {
-    private float speedInput = 0f;
+    private float _speedInput = 0f;
 
     public float cameraSpeed;
     // Start is called before the first frame update
@@ -17,13 +17,13 @@ public class CameraControll : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position += new Vector3(speedInput * cameraSpeed, 0, 0);
+        var targetPosition = transform.position + new Vector3(_speedInput * cameraSpeed, 0f, 0f);
+        transform.position = targetPosition;
     }
 
     public void MoveCamera(InputAction.CallbackContext context)
     {
         UnityEngine.Debug.Log($"Speed is set to {context.ReadValue<float>()}");
-        speedInput = context.ReadValue<float>();
-
+        _speedInput = context.ReadValue<float>();
     }
 }
