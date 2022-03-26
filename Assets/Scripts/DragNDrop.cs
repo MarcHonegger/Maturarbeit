764b399b2e03f8c86bc1f77f97226bd6dbf123e1
Polyphagus <44725652+Polyphagus@users.noreply.github.com>
2022-03-26 21:14:59 +0100

    Cards + Drag n Drop
    +Cards
    +Drag and Drop
    +Red Slime ^_^
    +Bugfix

---
+++ Assets/Scripts/DragNDrop.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragNDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+
+    private RectTransform rectTransform;
+    [SerializeField] private Canvas canvas;
+
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Start is called before the first frame update
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log($"BeginDrag");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log($"EndDrag");
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log($"Drag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        UnityEngine.Debug.Log($"Down");
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
+    
