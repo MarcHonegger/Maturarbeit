88b8287bb9e6c86fafd09db661a77869ce679ab5
MarcHonegger <marchonegger@icloud.com>
2022-04-28 14:10:15 +0200

    GameManager
    Singleton
    Manages things for both players (SpawnManager & AudioManager)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    
+    public AudioManager audioManager;
+    public SpawnManager spawnManager;
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        audioManager = GetComponentInChildren<AudioManager>();
+        spawnManager = GetComponentInChildren<SpawnManager>();
+    }
+}
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,7 +7,7 @@ public class SpawnManager : MonoBehaviour
 {
     public List<Transform> spawnPoints;
 
-    // If a troop is in this area (around Spawnpoint), there can not be spawned another one
+    // If a troop is in this area (around SpawnPoint), there can not be spawned another one
     public Vector3 spawnAreaSize;
 
     private void Start()
@@ -20,7 +20,14 @@ public class SpawnManager : MonoBehaviour
 
     public void Spawn(GameObject troopPrefab, int lane, bool isPlayerLeft)
     {
-        var troopGameObject = Instantiate(troopPrefab, spawnPoints[lane + (isPlayerLeft ? 0 : 4)].position, Quaternion.identity);
+        var spawnPosition = spawnPoints[lane + (isPlayerLeft ? 0 : 4)].position;
+        // SpawnPosition has to be shifted because the SpawnPoint of the troop is not 0|0|0
+        var spawnOffset = troopPrefab.transform.GetChild(0).position;
+        
+        GameObject troopGameObject = Instantiate(troopPrefab, spawnPosition - spawnOffset, Quaternion.identity);
         troopGameObject.tag = isPlayerLeft ? "LeftPlayer" : "RightPlayer";
+        
+        troopGameObject.transform.RotateAround(troopGameObject.transform.GetChild(0).position, Vector3.right, 45);
+
     }
 }
