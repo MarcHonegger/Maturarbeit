aa3d67a9bc795d7dd598c727ac5a510d66806b2c
MarcHonegger <marchonegger@icloud.com>
2022-04-30 22:49:48 +0200

    Update CameraControl.cs
    Left end

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,14 +7,15 @@ public class CameraControl : MonoBehaviour
 {
     private float _speedInput = 0f;
     public float cameraSpeed;
-    
+    public float leftEnd;
+
     void FixedUpdate()
     {
         var cameraTransform = transform;
         var cameraPosition = cameraTransform.position;
         var targetPosition = cameraPosition + new Vector3(_speedInput * cameraSpeed * 4f, 0f, 0f); 
         //limiting camera movement left side
-        if (cameraPosition.x <= -5f && _speedInput == -1f)
+        if (cameraPosition.x <= leftEnd && _speedInput == -1f)
         {
             UnityEngine.Debug.Log($"Limited Camera Left Side");
             return;
