d6421d83456bc52f73b25438d5a670a4927e066c
Polyphagus <44725652+Polyphagus@users.noreply.github.com>
2022-05-02 18:48:38 +0200

    play card to ground
    card detects ground

--- Assets/Scripts/Game/Card/DragNDrop.cs
+++ Assets/Scripts/Game/Card/DragNDrop.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class DragNDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
 
     private RectTransform rectTransform;
     [SerializeField] private Canvas canvas;
-
+    [SerializeField] private Camera cam;
+    [SerializeField] public GameObject spawnPointPrefab;
 
     private void Awake()
     {
@@ -24,7 +26,33 @@ public class DragNDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log($"EndDrag");
-        Debug.Log($"{rectTransform.position}");
+        
+        Vector3 point = new Vector3();
+        Event   currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+        
+        
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        //int layerMask = 1 << 7;
+        //layerMask = ~layerMask;
+        Transform transform = cam.transform;
+        LayerMask layermask = 1 << 7;
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+        //Debug.Log(ray);
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        //Debug.Log(point);
+        if (Physics.Raycast(ray, out hit,  Mathf.Infinity, layermask))
+        {
+            Vector3 worldPoint = hit.point;
+            var xpoint = worldPoint.y;
+            //Debug.Log(hit.point);
+            Instantiate(spawnPointPrefab, worldPoint, Quaternion.Euler(45f, 0f, 0f));
+        }
+
+        
+        
     }
     public void OnDrag(PointerEventData eventData)
     {
@@ -38,6 +66,8 @@ public class DragNDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, 
     }
     void Start()
     {
+        
+        cam = Camera.main;
 
     }
 
