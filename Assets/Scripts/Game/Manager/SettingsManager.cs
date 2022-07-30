171792b3eca677521a026689d9c2917d443519e9
MarcHonegger <marchonegger@icloud.com>
2022-07-30 10:29:58 +0200

    Glossar and Anforderungen

--- Assets/Scripts/Game/Manager/SettingsManager.cs
+++ Assets/Scripts/Game/Manager/SettingsManager.cs
@@ -2,15 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
-    public Slider volumeSlider;
+    public AudioMixer audioMixer;
 
     public void LoadStartPage()
     {
         SceneManager.LoadScene("StartPageScene");
+    }
+
+    public void SetFullscreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 }
